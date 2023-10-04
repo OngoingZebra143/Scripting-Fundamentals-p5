30ccf053874b933c2abce65cb962dfd7cb4f9138
OngoingZebra143 <sellinginstagram1223@gmail.com>
2023-10-04 13:13:18 -0700

    update the variable file

--- Scripting Examples/Assets/Scripts/Variables.cs
+++ Scripting Examples/Assets/Scripts/Variables.cs
@@ -12,16 +12,29 @@ public class Variables : MonoBehaviour
 
     // private Global/Member Variables
     private int _collectibleAmount = 0;
+    private float _horizontalInput = 0f;
+    private string _userName = "john007";
+    private bool _isGameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // make sure Score Variable is = 0 
+        score = 0; // assigning a Global/Member variable
+
+        // declare a local variable to track Start Position
+        Vector3 startPosition = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // trying to update a local variables value
+        // we can not update a local variable
+        //startPoision = new Vector3(Vector3.right);
+
+        // create a variable to determine object key movement
+        float verticalInput = Input.GetAxis("Vertical");
     }
 }
